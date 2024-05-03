e4e4639e6fcab245690a6c5bfb3d9644fbb67633
Gauravjot Garaya <gauravjot@outlook.com>
2024-05-02 23:09:07 -0700

    fix for user auth

--- src/services/user/fetch_info.ts
+++ src/services/user/fetch_info.ts
@@ -12,16 +12,18 @@ export const queryFetchUserInfo = (auth_token: string) => {
 	return useQuery({
 		queryKey: ["user-info" + auth_token.slice(0, 4)],
 		queryFn: () => {
-			return axios
-				.get(EP_USER_INFO, {
-					headers: {
-						"Content-Type": "application/json",
-						Authorization: auth_token,
-					},
-				})
-				.then((res) => {
-					return res.data as UserInfoType;
-				});
+			return auth_token.length > 0
+				? axios
+						.get(EP_USER_INFO, {
+							headers: {
+								"Content-Type": "application/json",
+								Authorization: auth_token,
+							},
+						})
+						.then((res) => {
+							return res.data as UserInfoType;
+						})
+				: Promise.reject("No auth token provided");
 		},
 	});
 };
